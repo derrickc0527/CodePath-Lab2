3a4d292cbb943df8328ca530477fe8905c0f259e
Derrick <derrickc0527@gmail.com>
2017-02-09 17:25:58 -0500

    upload last week code into repo

---
+++ My_Tumbly/My_Tumbly/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  My_Tumbly
+//
+//  Created by Derrick Chong on 2/3/17.
+//  Copyright © 2017 DerrickCorp. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var imagewindow: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
